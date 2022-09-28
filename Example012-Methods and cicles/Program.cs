4e2371558305c830a123108642064806562f6dff
SvetaF <feyginas@gmail.com>
2022-09-28 20:40:20 +0300

    Добавила задачи из 3 лекции

---
+++ Example012-Methods and cicles/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "+
+"ежели бы вас послали вместо нашего милого " +
+"Винценгероде, вы бы взяли приступом согласие "+
+"прусского короля. Вы так красноречивы. Вы "+
+"дадите мне чаю?";
+
+string newText = Replace(text, 'к', 'К');
+Console.WriteLine(newText);
+
+string Replace(string txt, char oldChar, char newChar)
+{
+    string result = string.Empty;
+    for(int i = 0; i < text.Length; i++){
+        if(text[i] == oldChar) result = result + $"{newChar}";
+        else result= result + $"{txt[i]}";
+    }
+
+    return result;
+}
---
+++ Example013-SortArray/Program.cs
@@ -0,0 +1,52 @@
+﻿// Создать и отсортировать массив
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] newArray = FillArray(size);
+PrintArray(newArray);
+Console.WriteLine();
+int[] sortNewArray = SortArray(newArray);
+PrintArray(sortNewArray);
+
+
+int[] FillArray(int n)
+{
+    int[] array = new int[n];
+    int index = 0;
+    while(index < n)
+    {
+        array[index] = new Random().Next(1, 10);
+        index ++;
+    }
+    return array;
+}
+
+void PrintArray(int[] col)
+{
+    int index = 0;
+    while(index < col.Length)
+    {
+        Console.Write(col[index] + ", ");
+        index++;
+    }
+
+   
+}
+
+int[] SortArray(int[] sort)
+{
+    int length = sort.Length;
+    for(int i = 0; i < length - 1; i++){
+    
+        int minIndex = i;
+        for(int j=i+1; j<length; j++ ){
+            if(sort[j] < sort[minIndex]) minIndex = j;
+            
+        }
+        int temp = sort[minIndex];
+        sort[minIndex] = sort[i];
+        sort[i] = temp;
+    }
+
+    return sort;
+}
